130ce46d4dea73efa206d23b5f56eab0034b1f31
ertu96 <48797125+ertu96@users.noreply.github.com>
2024-05-19 12:25:13 +0200

    6:08

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -18,17 +18,17 @@ const Footer = () => {
     const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data)
 
     return (
-        <footer className="m-10 mt-16 flex flex-col items-center rounded-2xl bg-dark text-light dark:bg-accentDark/90 dark:text-dark">
+        <footer className="sm:m-10 m-2 mt-16 flex flex-col items-center rounded-2xl bg-dark text-light dark:bg-accentDark/90 dark:text-dark">
             <h3 className="lg:text-4xl sm:text-3xl mt-16 px-4 text-center text-2xl font-medium capitalize dark:font-bold">
                 Interesting Stories | Updates | Guides
             </h3>
-            <p className="sm:text-base sm:w-3/5 mt-5 w-full px-4 text-center text-xs font-light">
+            <p className="sm:text-base sm:w-3/5 mt-5 w-full px-4 text-center text-xs font-light dark:font-medium">
                 Subscribe to learn about new technology and updates. Join over
                 5000+ members community to stay up to date with latest news.
             </p>
             <form
                 onSubmit={handleSubmit(onSubmit)}
-                className="mx-4 mt-6 flex min-w-[384px] items-stretch rounded bg-light p-2"
+                className="sm:min-w-[384px] sm:p-2 mx-4 mt-6 flex w-fit items-stretch rounded bg-light p-1 dark:bg-dark"
             >
                 <input
                     type="email"
@@ -37,12 +37,12 @@ const Footer = () => {
                         required: true,
                         pattern: /^\S+@\S+$/i,
                     })}
-                    className="mr-2 w-full border-0 border-b bg-transparent pb-1 text-dark focus:border-dark focus:ring-0"
+                    className="sm:pl-0 mr-2 w-full border-0 border-b bg-transparent pb-1 pl-2 text-dark focus:border-dark focus:ring-0"
                 />
 
                 <input
                     type="submit"
-                    className="cursor-pointer rounded bg-dark px-5 py-1 pl-0 font-medium text-light"
+                    className="sm:px-5 cursor-pointer rounded bg-dark px-3 py-1 pl-0 font-medium text-light dark:bg-light dark:text-dark"
                 />
             </form>
             <div className="mt-8 flex items-center">
@@ -62,7 +62,7 @@ const Footer = () => {
                     href={siteMetadata.github}
                     className="mr-4 inline-block h-6 w-6"
                 >
-                    <GithubIcon className="ease transition-all duration-200 hover:scale-125" />
+                    <GithubIcon className="ease fill-light transition-all duration-200 hover:scale-125 dark:fill-dark" />
                 </a>
                 <a
                     href={siteMetadata.dribbble}
@@ -71,11 +71,14 @@ const Footer = () => {
                     <DribbbleIcon className="ease transition-all duration-200 hover:scale-125" />
                 </a>
             </div>
-            <div className="relative mt-24 flex w-full flex-row items-center justify-between border-t border-solid border-light px-8 py-6 font-medium">
+            <div className="md:mt-24 md:flex-row relative mt-16 flex w-full flex-col items-center justify-between border-t border-solid border-light px-8 py-6 font-medium">
                 <span className="text-center">
                     &copy; 2023 CodeBucks. All rights reserved.
                 </span>
-                <Link className="text-center underline" href="/sitemap.xml">
+                <Link
+                    className="md:my-0 my-4 text-center underline"
+                    href="/sitemap.xml"
+                >
                     sitemap.xml
                 </Link>
                 <div className="text-center">
